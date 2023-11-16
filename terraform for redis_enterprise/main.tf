c61e320b83d61a3c1026e4ec387b2ba4f344a58d
Vishalsagarkoduru <135941640+Vishalsagarkoduru@users.noreply.github.com>
2023-11-16 17:38:59 +0530

    Update main.tf

--- terraform for redis_enterprise/main.tf
+++ terraform for redis_enterprise/main.tf
@@ -1,60 +1,40 @@
 #creation of resource group
-data "azurerm_client_config" "current" {}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "briliorg12"
+  name     = "myresourcegroup.eh"
   location = "East US2"
 }
+# Creation Vitual Network along with the address space
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myeventhubvnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
 
-# Create a managed identity for encryption key access
-resource "azurerm_user_assigned_identity" "userassignedid" {
-  location            = "eastus2"
-  name                = "brilliomi1221"
+# Creation of Subnet for the postgres server.
+resource "azurerm_subnet" "subnet" {
+  name                 = "myeventhubsubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  
+}
+
+# Creation of Private DNS zone. 
+
+resource "azurerm_private_dns_zone" "dnszone" {
+  name                = "example.myeventhub.azure.com"
   resource_group_name = azurerm_resource_group.rg.name
 }
-# Creation of keyvault With Key permissions
 
-resource "azurerm_key_vault" "brilliokv" {
-  name                       = "brilliokv112"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "premium"
-  soft_delete_retention_days = 7
-  purge_protection_enabled = true
-}
-resource "azurerm_key_vault_access_policy" "server" {
-  key_vault_id = azurerm_key_vault.brilliokv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.userassignedid.principal_id
+# Creation of Virtual network link. 
 
-  key_permissions = ["Get", "List", "WrapKey", "UnwrapKey", "GetRotationPolicy", "SetRotationPolicy"]
-  secret_permissions = ["Get","List",]
-}
-# Creation Of key in the keyvault. 
-resource "azurerm_key_vault_key" "generated" {
-  name         = "generatedcertificate"
-  key_vault_id = azurerm_key_vault.brilliokv.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-
-  rotation_policy {
-    automatic {
-      time_before_expiry = "P30D"
-    }
-
-    expire_after         = "P90D"
-    notify_before_expiry = "P29D"
-  }
+resource "azurerm_private_dns_zone_virtual_network_link" "vnetlink" {
+  name                  = "myeventhubVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.dnszone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  resource_group_name   = azurerm_resource_group.rg.name
 }
 
 #creation of Manages a Redis Enterprise Cluster.
@@ -62,34 +42,41 @@ resource "azurerm_redis_enterprise_cluster" "example" {
   name                = "Sampledemocache"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-
+  
   sku_name = "Enterprise_E20-4"
 }
-#Creation of Manages a Redis Enterprise Database.
-resource "azurerm_redis_enterprise_database" "example" {
-  name                = "default"
+resource "azurerm_private_endpoint" "acre" {
+  name                = "redisdns"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  cluster_id        = azurerm_redis_enterprise_cluster.example.id
-  client_protocol   = "Encrypted"
-  clustering_policy = "EnterpriseCluster"
-  eviction_policy   = "NoEviction"
-  port              = 10000
+  subnet_id           = azurerm_subnet.subnet.id
 
- identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.userassignedid.id]  
+  private_service_connection {
+    name                           = "Redis.sc"
+    private_connection_resource_id = azurerm_redis_enterprise_cluster.example.id
+    is_manual_connection           = false
+    subresource_names              = ["redisEnterprise"]
   }
-  customer_managed_key {
-    key_vault_key_id                    = azurerm_key_vault_key.generated.id
-    primary_user_assigned_identity_id   = azurerm_user_assigned_identity.userassignedid.id
-  }
-  linked_database_id = [
-    "${azurerm_redis_enterprise_cluster.example.id}/databases/default",
-  ]
 }
-/*
-    customer_managed_key {
-    key_vault_key_id                    = azurerm_key_vault_key.generated.id
-    primary_user_assigned_identity_id   = azurerm_user_assigned_identity.userassignedid.id
-    }
-    */
+
+#Creation of Manages a Redis Enterprise Database.
+resource "azapi_resource" "symbolicname" {
+  type = "Microsoft.Cache/redisEnterprise/databases@2022-01-01"
+  name = "default"
+  parent_id = azurerm_redis_enterprise_cluster.example.id
+  body = jsonencode({
+    properties = {
+      clientProtocol = "Encrypted"
+      clusteringPolicy = "EnterpriseCluster"
+      evictionPolicy = "NoEviction"
+      geoReplication = {
+        groupNickname = "newrcdatabase"
+         linkedDatabases = [
+          {
+            id = "${azurerm_redis_enterprise_cluster.example.id}/databases/default"
+          }
+        ]
+      }
+      }
+    })
+  }
