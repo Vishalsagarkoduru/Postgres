f6785dd3326829403285f783f7fdba9fc1bb5d1a
Vishalsagarkoduru <135941640+Vishalsagarkoduru@users.noreply.github.com>
2023-11-21 16:18:12 +0530

    Update main.tf

--- terraform for redis_enterprise/main.tf
+++ terraform for redis_enterprise/main.tf
@@ -10,6 +10,12 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
+# Creation Vitual Network along with the address space in other region
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "myeventhubvnet3"
+  location            = "West US2"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 
 # Creation of Subnet for the postgres server.
 resource "azurerm_subnet" "subnet" {
@@ -18,23 +24,13 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
-  
 }
-
-# Creation of Private DNS zone. 
-
-resource "azurerm_private_dns_zone" "dnszone" {
-  name                = "example.myeventhub.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Creation of Virtual network link. 
-
-resource "azurerm_private_dns_zone_virtual_network_link" "vnetlink" {
-  name                  = "myeventhubVnetZone.com"
-  private_dns_zone_name = azurerm_private_dns_zone.dnszone.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-  resource_group_name   = azurerm_resource_group.rg.name
+resource "azurerm_subnet" "subnet1" {
+  name                 = "myeventhubsubnet3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.4.0/24"]
+  service_endpoints    = ["Microsoft.Storage"] 
 }
 
 #creation of Manages a Redis Enterprise Cluster.
@@ -59,6 +55,26 @@ resource "azurerm_private_endpoint" "acre" {
   }
 }
 
+#creation of Manages a Redis Enterprise Cluster in other region.
+resource "azurerm_redis_enterprise_cluster" "example1" {
+  name                = "Sampledemocache3"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "West US2"
+  sku_name = "Enterprise_E20-4"
+}
+resource "azurerm_private_endpoint" "acre1" {
+  name                = "redisdns3"
+  location            = "West US2"
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet1.id
+
+  private_service_connection {
+    name                           = "Redis.sc3"
+    private_connection_resource_id = azurerm_redis_enterprise_cluster.example1.id
+    is_manual_connection           = false
+    subresource_names              = ["redisEnterprise"]
+  }
+
 #Creation of Manages a Redis Enterprise Database.
 resource "azapi_resource" "symbolicname" {
   type = "Microsoft.Cache/redisEnterprise/databases@2022-01-01"
@@ -80,3 +96,29 @@ resource "azapi_resource" "symbolicname" {
       }
     })
   }
+#Creation of Manages a Redis Enterprise Database in other region.
+resource "azapi_resource" "symbolicname3" {
+  type = "Microsoft.Cache/redisEnterprise/databases@2022-01-01"
+  name = "default"
+  parent_id = azurerm_redis_enterprise_cluster.example1.id
+  body = jsonencode({
+    properties = {
+      clientProtocol = "Encrypted"
+      clusteringPolicy = "EnterpriseCluster"
+      evictionPolicy = "NoEviction"
+      geoReplication = {
+        groupNickname = "newrcdatabase"
+     
+         linkedDatabases = [
+          {
+            id = "${azurerm_redis_enterprise_cluster.example.id}/databases/default"
+            id = "${azurerm_redis_enterprise_cluster.example1.id}/databases/default"         
+          }
+        ]
+      }
+     }
+    })
+  }
+
+# Note :- persistence cannot be used at the same time as active geo-replication. If an active geo-replicated 
+# cache goes down, the instance will restore from the geo-replicated copies rather than a copy saved to disk.
